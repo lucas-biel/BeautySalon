46ca565e95a818bf4a61e8c9ab594cbc19b7a641
lucas-biel <santos.lucas1029@gmail.com>
2022-05-21 09:29:39 -0300

    add função de esconder menu

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/* ========== abre e facha o menu ==========*/
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for(const element of toggle) {
   element.addEventListener('click', function() {
     nav.classList.toggle('show')
+  })
+}
+
+/* ========== esconder o menu ==========*/
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
   })
 }
