74a5534e68bcca28c1ae88a7ac066f9b126f22d3
lucas-biel <santos.lucas1029@gmail.com>
2022-05-21 17:41:03 -0300

    sombrear header

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle) {
+for (const element of toggle) {
   element.addEventListener('click', function() {
     nav.classList.toggle('show')
   })
@@ -11,8 +11,20 @@ for(const element of toggle) {
 /* ========== esconder o menu ==========*/
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links) {
+for (const link of links) {
   link.addEventListener('click', function() {
     nav.classList.remove('show')
   })
 }
+
+/* ========== sombrear o header ==========*/
+const header = document.querySelector('#header')
+const navHeight =header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
