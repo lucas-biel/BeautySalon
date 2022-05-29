d605472df8fc6060448c2b49bf95c03e50c414de
lucas-biel <santos.lucas1029@gmail.com>
2022-05-29 17:06:51 -0300

    adição da função activateMenu[...]

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 // ========== sombrear o header ==========
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -29,7 +29,7 @@ function changeHeaderWhenScroll() {
   }
 }
 
-// ========== testimonials carousel ==========
+// ========== carrossel de depoimentos ==========
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,  //quantos slides aparecem
   pagination: {
@@ -65,9 +65,9 @@ scrollReveal.reveal(
   )
 
 // botão voltar para o topo
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -75,7 +75,35 @@ function backToTop() {
   }
 }
 
+// menu ativo conforme seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
+}
+
+// when scroll
 window.addEventListener('scroll', function() {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
